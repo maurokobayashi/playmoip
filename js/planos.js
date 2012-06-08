a2ea237a8adfd79594055a363f784cbf97d7bbc2
Mauro Kobayashi <mauro.kobayashi@gmail.com>
2012-06-08 14:15:21 -0300

    extraindo logicas duplicadas do js de planos

--- js/planos.js
+++ js/planos.js
@@ -1,53 +1,51 @@
+var montarStatus = function(plano) {
+
+	var labelStatus = '';
+
+	if(plano.status == 'ativo') {
+		labelStatus = '<span class="label label-success">ativo</span>';
+	}
+	else if (plano.status == 'inativo') {
+		labelStatus = '<span class="label label-error">inativo</span>';
+	}
+	else if (plano.status == 'expirado') {
+		labelStatus = '<span class="label label-error">expirado</span>';
+	}
+	else {
+		labelStatus = '<span class="label label-inverse">desconhecido</span>';
+	}
+	
+	return labelStatus;
+};
+
+var montarPreco = function(plano) {
+
+	var preco = 'R$';
+	preco += plano.price/100;
+
+	return preco;
+};
+
+var montarPeriodicidade = function(plano) {
+
+	var periodicidade = plano.periodicity.length;
+	
+	if(plano.periodicity.unit == 'day') {
+		periodicidade += ' dia(s)';
+	}
+	else if(plano.periodicity.unit == 'month') {
+		periodicidade += ' m&ecirc;s(es)';
+	}
+	else if(plano.periodicity.unit == 'year') {
+		periodicidade += 'ano(s)';
+	}
+	
+	return periodicidade;
+};
+
 $(document).ready(function() {
 
 	$('#tabsPlanosHeader a:first').tab('show');
-	
-	
-	var montarStatus = function(plano) {
-	
-		var labelStatus = '';
-	
-		if(plano.status == 'ativo') {
-			labelStatus = '<span class="label label-success">ativo</span>';
-		}
-		else if (plano.status == 'inativo') {
-			labelStatus = '<span class="label label-error">inativo</span>';
-		}
-		else if (plano.status == 'expirado') {
-			labelStatus = '<span class="label label-error">expirado</span>';
-		}
-		else {
-			labelStatus = '<span class="label label-inverse">desconhecido</span>';
-		}
-		
-		return labelStatus;
-	};
-	
-	var montarPreco = function(plano) {
-	
-		var preco = 'R$';
-		preco += plano.price/100;
-	
-		return preco;
-	};
-	
-	var montarPeriodicidade = function(plano) {
-	
-		var periodicidade = plano.periodicity.length;
-		
-		if(plano.periodicity.unit == 'day') {
-			periodicidade += ' dia(s)';
-		}
-		else if(plano.periodicity.unit == 'month') {
-			periodicidade += ' m&ecirc;s(es)';
-		}
-		else if(plano.periodicity.unit == 'year') {
-			periodicidade += 'ano(s)';
-		}
-		
-		return periodicidade;
-	};
-	
 	
 	/*
 	getAllPlans.done(function(msg) {
