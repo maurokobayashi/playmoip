e63ebf400e8f3448d40cb4225da66ec66a4e6d76
Mauro Kobayashi <mauro.kobayashi@gmail.com>
2012-06-08 11:49:24 -0300

    implementando chamadas assincronas via jQuery

--- js/getPlanos.js
+++ js/getPlanos.js
@@ -4,9 +4,8 @@ var getAllPlans = $.ajax({
   url: url,
   type: 'GET',
   dataType: 'json',
-//   jsonpCallback: 'cbk',
-//   crossDomain: true,
-//   isLocal: true,
+  //jsonpCallback: 'cbk',
+  //crossDomain: true,
   statusCode: {
     404: function() {
       alert("404: page not found");
@@ -15,9 +14,101 @@ var getAllPlans = $.ajax({
   timeout: 30000 //30 segundos
 });
 
-getAllPlans.success(function(data, textStatus, jqXHR) {
-  alert('SUCCESS: ' + textStatus + '\nJSON:\n' + JSON.stringify(data));
+getAllPlans.success(function(planos, textStatus, jqXHR) {
+
+	console.log(JSON.stringify(planos));
+	
+	var status='';
+	var preco = '';
+	var periodicidade = '';
+	
+	var retorno='';
+	retorno += '<table class="table table-striped">'
+	retorno += '    <thead>'
+	retorno += '        <tr>'
+	retorno += '            <th>c&oacute;digo</th>'
+	retorno += '            <th>nome</th>'
+	retorno += '            <th>descri&ccedil;&atilde;o</th>'
+	retorno += '            <th>criado em</th>'
+	retorno += '            <th>pre&ccedil;o</th>'
+	retorno += '            <th>periodicidade</th>'
+	retorno += '            <th>status</th>'
+	retorno += '        </tr>'
+	retorno += '    </thead>'
+	retorno += '    <tbody>'
+	
+	$.each(planos, function(i, plano) {
+	
+		status = montarStatus(plano);
+		preco = montarPreco(plano);
+		periodicidade = montarPeriodicidade(plano);
+		
+		retorno += '    <tr>'
+		retorno += '        <td id="id">'+plano.plan_code+'</td>'
+		retorno += '        <td id="bandeira">'+plano.name+'</td>'
+		retorno += '        <td>'+plano.description+'</td>'
+		retorno += '        <td>'+plano.created_at+'</td>'
+		retorno += '        <td>'+preco+'</td>'
+		retorno += '        <td>'+periodicidade+'</td>'
+		retorno += '        <td>'+plano.afiliacao+'</td>'
+		retorno += '        <td>'+status+'</td>'
+		retorno += '    </tr>'
+    });
+    
+    retorno += '    <tbody>'
+	retorno += '</table>'
+
+	$("#results").html(retorno);
 });
+
+var monstarStatus = function(plano) {
+
+	var labelStatus = '';
+
+	if(plano.status == 'ativo') {
+		labelStatus = '<span class="label label-success">ativo</span>';
+	}
+	else if (plano.status == 'inativo') {
+		labelStatus = '<span class="label label-error">inativo</span>';
+	}
+	else if (plano.status == 'expirado') {
+		labelStatus = '<span class="label label-error">expirado</span>';
+	}
+	else {
+		labelStatus = '<span class="label label-inverse">desconhecido</span>';
+	}
+	
+	return labelStatus;
+};
+
+var montarPreco = function(plano) {
+
+	var preco = 'R$';
+	preco += plano.price;
+
+	return preco;
+};
+
+var montarPeriodicidade = function(plano) {
+
+	var periodicidade = plano.periodicity.length;
+	
+	if(plano.periodicity.unit == 'day') {
+		periodicidade += ' dia(s)';
+	}
+	else if(plano.periodicity.unit == 'month') {
+		periodicidade += ' m&ecirc;s(es)';
+	}
+	else if(plano.periodicity.unit == 'year') {
+		periodicidade += 'ano(s)';
+	}
+	
+	return periodicidade;
+};
+
+
+getAllPlans;
+
 
 /*
 getAllPlans.done(function(msg) {
@@ -41,4 +132,3 @@ getAllPlans.always(function() {
 	alert('ALWAYS');
 });
 */
-getAllPlans;
